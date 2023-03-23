44ea406de5d1be8d0205f7eb7ef0d397774be447
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-03-23 10:10:26 +0800

    Add function to save play count result and back to MainActivity

--- app/src/main/java/com/suhun/guessutils/RecordActivity.kt
+++ app/src/main/java/com/suhun/guessutils/RecordActivity.kt
@@ -2,15 +2,36 @@ package com.suhun.guessutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.suhun.guessutils.databinding.ActivityRecordBinding
 
 class RecordActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecordBinding
     val tag:String = RecordActivity::class.java.simpleName
+    val utils:Utils = Utils()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun saveGame(view: View){
+        val scoreCounter:Int = intent.getIntExtra("COUNT", -1)
+        val player:String = binding.playNameInput.text.toString()
+        val saveKey:String = "NAME_SCORE"
+        val fileName:String = "save_guess_score"
+        var saveData:String = "$player \t $scoreCounter"
+        var saveResult = utils.getSaveResultString(this,fileName, saveKey)
+        if(saveResult == null){
+            Log.d(tag,"Save result is null")
+            utils.saveResultString(this, fileName, saveKey, saveData)
+        }else{
+            Log.d(tag,"Already have data:$saveResult")
+            var tempSave:String = "$saveData \n $saveResult"
+            utils.saveResultString(this, fileName, saveKey, tempSave)
+        }
+        utils.goToAvtivity(this, MainActivity::class.java)
     }
 }
---
+++ app/src/main/java/com/suhun/guessutils/Utils.kt
@@ -0,0 +1,26 @@
+package com.suhun.guessutils
+
+import android.content.Intent
+import android.content.Context
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class Utils {
+    val tag:String = Utils::class.java.simpleName
+
+    fun getSaveResultString(contex:Context, fileName:String, saveKey:String):String?{
+        return contex.getSharedPreferences(fileName, AppCompatActivity.MODE_PRIVATE).getString(saveKey, null)
+    }
+
+    fun saveResultString(contex: Context, fileName: String, saveKey: String, saveData:String){
+        Log.d(tag, "Save data:$saveData")
+        contex.getSharedPreferences(fileName, AppCompatActivity.MODE_PRIVATE)
+            .edit().putString(saveKey,saveData)
+            .apply()
+    }
+
+    fun goToAvtivity(targetContext:Context, purpose:Class<*>){
+        var intent:Intent = Intent(targetContext, purpose)
+        targetContext.startActivity(intent)
+    }
+}
